47f123affff49c28334a7ef2a44cdcfc09e8360e
202018010428FrancisLiu <153816963+202018010428FrancisLiu@users.noreply.github.com>
2024-05-04 05:32:49 +0800

    Delete Backend code/smartMeter directory

--- Backend code/smartMeter/src/main/java/com/example/smartmeter/dao/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.smartmeter.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.smartmeter.entity.SysUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
--- Backend code/smartMeter/src/main/java/com/example/smartmeter/entity/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartmeter.entity;
-
-public class Constants {
-
-    public static final int SUCCESS_CODE = 0;
-    public static int ERROR_CODE = -1;
-}
